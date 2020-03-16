6ea65e21764aa7810d3801ea1bebfb4006967289
kirstyc <kirstycha@gmail.com>
2020-03-15 20:14:51 -0600

    changes from testing at uni

--- manualDriveTest.py
+++ manualDriveTest.py
@@ -2,7 +2,7 @@ from modules.arc_mc_ui.XboxCtrl import XboxCtrl
 from modules.arc_mc_components.throttle import Throttle
 from modules.arc_mc_components.stepper import Stepper
 from modules.arc_mc_components.rpi_interface import IO
-from modules.arc_mc_control_sys.interface import Steering, Speed
+from modules.arc_mc_ctrlsys.interfaces import Steering, Speed
 
 from time import sleep
 
@@ -12,10 +12,11 @@ def main():
     gpio = IO()
     throttle = Throttle(gpio, pin = 4, pwm = 0)
     throttle.setup()
-    stepper = Stepper(gpio, pinDir = 17, pinPUl = 27)
+    stepper = Stepper(gpio, pinDir = 17, pinPul = 27)
     stepper.setup()
     
-    while(1)
+    while(1):
+        sleep(0.5)
         if not controller.is_connected():
             controller.reconnect()
             print('reconnecting to controller')
@@ -29,9 +30,10 @@ def main():
             # read controller.get_steering_position() and write to stepper GPIO pin
             steeringPos = controller.get_steering_position()
             steeringAngle = Steering.joystickToSteeringAngle(steeringPos)
-            stepper.rotate(steeringAngle)
+            #stepper.rotate(steeringAngle)
             print(f"steeringPos: {steeringPos}, steeringAngle: {steeringAngle}")
         else:
+            print("Brake condition ",controller.check_brake())
             print("Brake pressed, deiniting ...")
             controller.deinit()
             throttle.off()
