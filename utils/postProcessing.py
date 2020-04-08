805e8d42e649d4e4b3c6ff23e612dc62d17083c1
Munifa <munifa.saeed@ucalgary.ca>
2020-04-08 15:43:42 -0600

    added subplot for displaying image frames

--- utils/postProcessing.py
+++ utils/postProcessing.py
@@ -5,16 +5,18 @@ import matplotlib.animation as animation
 import numpy as np
 
 class PostProcessing():
-    def __init__(self, speedsFileName = 'speedsList.pickle', anglesFileName = 'anglesList.pickle'):
-        self.speedsList = self.unpickle(speedsFileName)
+    def __init__(self, voltsFileName = 'voltsList.pickle', anglesFileName = 'anglesList.pickle', framesFileName = 'framesList.pickle'):
+        self.voltsList = self.unpickle(voltsFileName)
         self.anglesList = self.unpickle(anglesFileName)
-        self.max = min(len(self.speedsList), len(self.anglesList))
+        self.framesList = self.unpickle(framesFileName)
+        self.max = min(len(self.voltsList), len(self.anglesList))
         self.counter = 0
         self.fig = None
-        self.speedAxes = None
+        self.voltAxes = None
         self.angleAxes = None
+        self.framesAxes = None
         self.ani = None
-        plt.rcParams['animation.ffmpeg_path'] = '/ffmpeg-4.2.2-win64-static/bin/ffmpeg.exe'
+        plt.rcParams['animation.ffmpeg_path'] = 'C:/Users/munif/Desktop/ffmpeg-4.2.2-win64-static/bin/ffmpeg.exe'
 
     def unpickle(self, fileName):
         try:
@@ -34,7 +36,8 @@ class PostProcessing():
             return
            
         self.angleAxes.clear()
-        self.speedAxes.clear()
+        self.voltAxes.clear()
+        self.framesAxes.clear()
 
         self.angleAxes.set_title('Steering Angle')
         self.angleAxes.set_theta_zero_location('N')
@@ -42,19 +45,24 @@ class PostProcessing():
         self.angleAxes.set_ylim([0, 2])
         self.angleAxes.set_yticklabels([])
 
-        self.speedAxes.set_title('Bike Speed')
-        self.speedAxes.set_ylabel('Speed (m/s)')
-        self.speedAxes.set_xlabel('Some Increment')
-        self.speedAxes.set_xlim([0, 300])
-        self.speedAxes.set_ylim([0, 10])
-        self.speedAxes.grid(True)
+        self.voltAxes.set_title('Throttle Voltage')
+        self.voltAxes.set_ylabel('Throttle Voltage (V)')
+        self.voltAxes.set_xlabel('Some Increment??')
+        self.voltAxes.set_xlim([0, 300])
+        self.voltAxes.set_ylim([0, 10])
+        self.voltAxes.grid(True)
+
+        self.framesAxes.set_title('RealSense Camera Frame')
+        self.framesAxes.set_yticklabels([])
+        self.framesAxes.set_xticklabels([])
 
         timeX = [i for i in range(0, self.counter)]
-        speedY = self.speedsList[slice(self.counter)]
+        voltY = self.voltsList[slice(self.counter)]
         angleX, angleY = self.calcXYfromAngle(self.anglesList[self.counter])
 
         self.angleAxes.quiver(0, 0, angleX, angleY, color = ['m'], scale = 2)
-        self.speedAxes.plot(timeX, speedY)
+        self.voltAxes.plot(timeX, voltY)
+        self.framesAxes.imshow(self.framesList[self.counter])
 
         self.counter = self.counter + 1
 
@@ -66,8 +74,10 @@ class PostProcessing():
     
     def runAnimation(self, updateInterval, saveAs = 'animation.mp4'):
         self.fig = plt.figure()
-        self.angleAxes = self.fig.add_subplot(2,1,1, polar=True, projection='polar')
-        self.speedAxes = self.fig.add_subplot(2,1,2)
+        self.angleAxes = self.fig.add_subplot(3,1,2, polar=True, projection='polar')
+        self.voltAxes = self.fig.add_subplot(3,1,3)
+        self.framesAxes = self.fig.add_subplot(3,1,1)
+        plt.subplots_adjust(hspace = 0.3)
 
         self.ani = animation.FuncAnimation(self.fig, self.animate, interval=updateInterval ,blit=False, repeat=False, frames = self.max-1)
         #either save it or show it - don't do both - results in recursion stack errors
@@ -77,22 +87,34 @@ class PostProcessing():
             plt.show()
 
 if __name__ == "__main__":
+    import imageio
     numEntries = 90
-    speedsFileName = 'speedsList.pickle'
+    voltsFileName = 'voltsList.pickle'
     anglesFileName = 'anglesList.pickle'
+    framesFileName = 'framesList.pickle'
     
     anglesList = [i for i in range(-45, 45)]
-    speedsList = np.random.randint(low =0, high=7, size =numEntries)
-    
+    voltsList = np.random.randint(low =0, high=7, size =numEntries)
+    framesList = []
+    for i in range(-45,45):
+        if i%2 == 0:
+            framesList.append(imageio.imread(r'C:\Users\munif\Desktop\test2.png'))
+        else:
+            framesList.append(imageio.imread(r'C:\Users\munif\Desktop\test.png'))
+
     try:
-        with open(speedsFileName, 'wb') as pickleOut: #overwrites existing file
-            pickle.dump(speedsList, pickleOut, pickle.HIGHEST_PROTOCOL)
+        with open(voltsFileName, 'wb') as pickleOut: #overwrites existing file
+            pickle.dump(voltsList, pickleOut, pickle.HIGHEST_PROTOCOL)
         
         with open(anglesFileName, 'wb') as pickleOut:
             pickle.dump(anglesList, pickleOut, pickle.HIGHEST_PROTOCOL)
+
+        with open(framesFileName, 'wb') as pickleOut:
+            pickle.dump(framesList, pickleOut, pickle.HIGHEST_PROTOCOL)
+
     except pickle.PicklingError as e:
         print('An exception occured: ', e)
 
-    obj = PostProcessing(speedsFileName, anglesFileName)
+    obj = PostProcessing(voltsFileName, anglesFileName)
     obj.runAnimation(500, '') #specify how frequently it should grab new data and update plots
     
